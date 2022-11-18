13a601f98c3c14fda908f7efbb3c0b0623328971
Matthew Collins <m.collins@etoncollege.org.uk>
2022-11-18 16:45:06 +0000

    Initial Commit

---
+++ Challenge17/Challenge17App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge17App.swift
+//  Challenge17
+//
+//  Created by Collins, Matthew - MC on 18/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Challenge17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge17/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Challenge17
+//
+//  Created by Collins, Matthew - MC on 18/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
